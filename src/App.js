35627c0c754d98365046814b15e37a718b9768f1
taldr27 <diegogarciaq98@gmail.com>
2022-11-10 12:43:18 -0500

    Install react router dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
+import React from 'react';
+import {
+  Routes,
+  Route,
+} from 'react-router-dom';
 import './App.css';
 import Books from './pages/Books';
 import Navbar from './components/Navbar';
+import Categories from './pages/Categories';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Books />
+      <Routes>
+        <Route path="/" element={<Books />} />
+        <Route path="/categories" element={<Categories />} />
+      </Routes>
     </div>
   );
 }
