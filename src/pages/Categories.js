af8c1e61970a71e65825959a9b66a2f361c670c5
taldr27 <diegogarciaq98@gmail.com>
2022-11-17 10:07:36 -0500

    Add the api calls and the slicer

--- src/pages/Categories.js
+++ src/pages/Categories.js
@@ -1,6 +1,7 @@
+// import { createAsyncThunk } from '@reduxjs/toolkit';
+// import axios from 'axios';
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchBooks } from '../redux/books/Books';
 import { checkStatus } from '../redux/categories/Categories';
 import '../styles/Categories.css';
 
@@ -10,18 +11,6 @@ const Categories = () => {
   const onClickStatus = (event) => {
     dispatch(checkStatus());
     event.currentTarget.classList.toggle('btn-off');
-    // fetch('https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/EqZ6YUUbJz8PKUL0OiMU/books/', {
-    //   method: 'POST',
-    //   body: JSON.stringify({
-    //     item_id: 'item6', title: '123', author: '123', category: '123',
-    //   }),
-    //   headers: {
-    //     'Content-type': 'application/json; charset=UTF-8',
-    //   },
-    // })
-    //   .then((response) => response.text())
-    //   .then((json) => console.log(json));
-    console.log(dispatch(fetchBooks()));
   };
   return (
     <div className="big-container">
